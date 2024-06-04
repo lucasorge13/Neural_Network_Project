aa6187dd9dfc794cb33dbd6b0d02a5690de6732d
Lucas Sorge <lucasorge13@gmail.com>
2024-06-04 17:28:44 -0500

    passing data commit

--- p1.py
+++ p1.py
@@ -1,18 +1,23 @@
-inputs = [1, 2, 3, 2.5]
+import numpy as np
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+np.random.seed(0)
 
-biases = [2, 3, 0.5]
+#input data for the sample network
+X = [[1, 2, 3, 2.5],
+      [2.0, 5.0, -1.0, 2.0],
+      [-1.5, 2.7, 3.3, -0.8]]
 
-layer_outputs = []
-for neuron_weights, neuron_bias in zip(weights, biases):
-  neuron_output = 0
-  for n_input, weight in zip(inputs, neuron_weights):
-    neuron_output += n_input*weight
-  neuron_output += neuron_bias
-  layer_outputs.append(neuron_output)
+class Layer_Dense:
+  def __init__(self, n_inputs, n_neurons):
+    self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+    self.biases = np.zeros((1, n_neurons))
+  def forward(self, inputs):
+    self.output = np.dot(inputs, self.weights) + self.biases
 
-print(layer_outputs)
+layer1 = Layer_Dense(4, 5)
+layer2 = Layer_Dense(5, 2)
 
+layer1.forward(X)
+
+layer2.forward(layer1.output)
+print(layer2.output)
