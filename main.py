68b8585a17d8ae496ededd738f771ebe72e61979
Lucas Sorge <lucasorge13@gmail.com>
2024-06-04 19:27:27 -0500

    dataset initialization

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
 #input data for the sample network
 X = [[1, 2, 3, 2.5],
       [2.0, 5.0, -1.0, 2.0],
       [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
   def __init__(self, n_inputs, n_neurons):
@@ -14,10 +18,10 @@ class Layer_Dense:
   def forward(self, inputs):
     self.output = np.dot(inputs, self.weights) + self.biases
 
+class Activation_ReLU:
+  def forward(self, inputs):
+    self.output = np.maximum(0, inputs)
+
 layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
 
 layer1.forward(X)
-
-layer2.forward(layer1.output)
-print(layer2.output)
